897522103e46506410f7c88f0ef25245faf8bfdc
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-28 11:33:11 +0530

    [Devesh]: added board method

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
-
+	
+	char []board = new char[10];
+	
+	public void board() {
+		for(int i=1;i<board.length;i++) {
+			board[i] = ' ';
+		}
+	}
 }
