97a39499b8ebed70a1718530d7322ac9d8cea6ca
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-28 11:56:50 +0530

    [Devesh]: added input from user

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,12 +1,41 @@
 package com.bridgelabz.tictactoe;
 
-public class TicTacToeGame {
+public class TicTacToeGame {	
+	private static char player;
+	private static char computer;
 	
-	char []board = new char[10];
-	
-	public void board() {
+	public char[] createBoard() {
+		char []board = new char[10];
 		for(int i=1;i<board.length;i++) {
 			board[i] = ' ';
 		}
+		return board;
+	}
+	
+	public void setInput(char player) {
+		setPlayer(player);
+		if(getPlayer() == 'X') {
+			setPlayer('O');
+		}
+		else
+			setPlayer('X');
+		
+		
+	}
+
+	public static char getComputer() {
+		return computer;
+	}
+
+	public static void setComputer(char computer) {
+		TicTacToeGame.computer = computer;
+	}
+
+	public char getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(char player) {
+		TicTacToeGame.player = player;
 	}
 }
--- src/com/bridgelabz/tictactoe/TicTacToeGameMain.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGameMain.java
@@ -1,9 +1,17 @@
 package com.bridgelabz.tictactoe;
+import java.util.*;
+
 
 public class TicTacToeGameMain {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		TicTacToeGame t = new TicTacToeGame();
+		char[] board = t.createBoard();
+		System.out.println("Enter your Choice X or O");
+		char input = sc.next().charAt(0);
+		t.setInput(Character.toUpperCase(input));
+		System.out.println(t.getPlayer());
 	}
 
 }
